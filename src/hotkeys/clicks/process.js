15c20795df7af50fd40198865ae5ee81f793f6f9
Lefcott <leandro.cotti@widergy.com>
2020-10-12 04:40:07 -0300

    fix

--- src/hotkeys/clicks/process.js
+++ src/hotkeys/clicks/process.js
@@ -1,9 +1,8 @@
-// import robotjs from 'robotjs';
-
+import robotjs from 'robotjs';
 import iohook from 'iohook';
 
 iohook.on('mouseclick', ({ x, y }) => {
-  console.log(`#${robotjs.getPixelColor(x, y).toUpperCase()}`);
+  // console.log(`#${robotjs.getPixelColor(x, y).toUpperCase()}`);
 });
 
 iohook.start();
